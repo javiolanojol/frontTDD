b962579b79595cb3f66ce3e37985cdc3b90fbf03
Javi Olano <javiolanojol@gmail.com>
2021-03-02 22:45:28 +0100

    Esta viiiiiiiiiiiiiiiiivo viiiiiiiiiiiiiiiiiiiivo

--- app_tdd/src/store/index.js
+++ app_tdd/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import state from "/store/state";
+import state from "@/store/state";
 import mutations from "@/store/mutations";
 import actions from "@/store/actions";
 import getters from "@/store/getters";
